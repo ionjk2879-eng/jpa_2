efd6a91fd9da9c7bc2d0a1e8303fd632256af184
uiuxzero <uiuxzero@gmail.com>
2026-06-05 11:25:29 +0900

    jpa학습 3

--- src/main/java/com/example/jpa_exam/entity/Post.java
+++ src/main/java/com/example/jpa_exam/entity/Post.java
@@ -1,18 +1,32 @@
 package com.example.jpa_exam.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Table(name = "posts")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Post {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 100)
     private String title;
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
+    @Column(nullable = false)
     private String author;
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     public static Post create(String title,String content,String author) {
