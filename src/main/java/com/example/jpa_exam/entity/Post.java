8325f9a0b3883a485e60a193bb2f5226707bc742
uiuxzero <uiuxzero@gmail.com>
2026-06-05 10:30:25 +0900

    jpa학습2

--- src/main/java/com/example/jpa_exam/entity/Post.java
+++ src/main/java/com/example/jpa_exam/entity/Post.java
@@ -1,12 +1,25 @@
 package com.example.jpa_exam.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 
+@Entity
 public class Post {
+    @Id
     private Long id;
     private String title;
     private String content;
     private String author;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public static Post create(String title,String content,String author) {
+        Post post = new Post();
+        post.title=title;
+        post.content=content;
+        post.author=author;
+        return post;
+    }
 }
